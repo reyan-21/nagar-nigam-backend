f191dd2092dde559b7600d0fb3a399b81089b8bb
Rishub <rishub@Rishis-MacBook-Air.local>
2026-08-29 17:11:46 +0530

    Fixed SSL Certificate error

--- database.py
+++ database.py
@@ -1,11 +1,20 @@
 import os
+import ssl
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-# Yahan humne os.getenv lagaya hai taaki cloud par live DB use ho, aur Mac par local DB
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://root:password@localhost:3306/nagarnigam")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# Aiven Cloud ke SSL Certificate Error ko bypass karne ka logic
+custom_args = {}
+if "aivencloud" in SQLALCHEMY_DATABASE_URL:
+    ctx = ssl.create_default_context()
+    ctx.check_hostname = False
+    ctx.verify_mode = ssl.CERT_NONE
+    custom_args = {"ssl": ctx}
+
+# engine mein custom_args pass kar diye
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args=custom_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
