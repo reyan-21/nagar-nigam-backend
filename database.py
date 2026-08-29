57e170c7360e709f56b4b338eed3475aba134373
Rishub <rishub@Rishis-MacBook-Air.local>
2026-08-29 16:39:17 +0530

    Fixed database URL for Render

--- database.py
+++ database.py
@@ -1,9 +1,10 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
-# Format: mysql+pymysql://username:password@localhost/database_name
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:@localhost/nagar_nigam_db"
+# Yahan humne os.getenv lagaya hai taaki cloud par live DB use ho, aur Mac par local DB
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://root:password@localhost:3306/nagarnigam")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
